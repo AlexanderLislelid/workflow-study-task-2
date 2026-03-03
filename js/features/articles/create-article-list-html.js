d128f0f652d7ca7c68a0714405d9693760320f0e
AlexanderLislelid <alexlislelid@gmail.com>
2026-03-04 00:48:02 +0100

    refactor article card favorite button to use SVG icon functions

--- js/features/articles/create-article-list-html.js
+++ js/features/articles/create-article-list-html.js
@@ -1,8 +1,5 @@
-import {
-  getFavourites,
-  isFavourite,
-  toggleFavourite,
-} from "../../utils/favourites.js";
+import { isFavourite, toggleFavourite } from "../../utils/favourites.js";
+import { heartOutline, filledHeart } from "../../ui/icons/svg.js";
 
 export default function createArticleListHtml(container, articles) {
   if (articles.length === 0) {
@@ -29,15 +26,11 @@ function createArticleCard(article) {
   favBtn.type = "button";
   favBtn.className =
     "absolute top-2 right-2 z-10 p-1 bg-white/80 rounded-full hover:bg-white shadow";
-  favBtn.innerHTML = isFavourite(id)
-    ? `<svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' class='w-6 h-6 text-red-500'><path fill-rule='evenodd' d='M12.1 4.435c1.74-1.72 4.56-1.72 6.3 0 1.72 1.72 1.72 4.52 0 6.24l-6.07 6.06a.75.75 0 0 1-1.06 0l-6.07-6.06c-1.72-1.72-1.72-4.52 0-6.24 1.74-1.72 4.56-1.72 6.3 0Z' clip-rule='evenodd'/></svg>`
-    : `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' class='w-6 h-6 text-red-500'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M16.5 3.75a5.25 5.25 0 0 1 3.71 8.96l-7.07 7.07a.75.75 0 0 1-1.06 0l-7.07-7.07a5.25 5.25 0 1 1 7.42-7.42l.42.42.42-.42a5.25 5.25 0 0 1 3.63-1.54Z'/></svg>`;
+  favBtn.innerHTML = isFavourite(id) ? heartOutline() : filledHeart();
   favBtn.addEventListener("click", function (e) {
     e.preventDefault();
     toggleFavourite(id, article);
-    favBtn.innerHTML = isFavourite(id)
-      ? `<svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' class='w-6 h-6 text-red-500'><path fill-rule='evenodd' d='M12.1 4.435c1.74-1.72 4.56-1.72 6.3 0 1.72 1.72 1.72 4.52 0 6.24l-6.07 6.06a.75.75 0 0 1-1.06 0l-6.07-6.06c-1.72-1.72-1.72-4.52 0-6.24 1.74-1.72 4.56-1.72 6.3 0Z' clip-rule='evenodd'/></svg>`
-      : `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' class='w-6 h-6 text-red-500'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M16.5 3.75a5.25 5.25 0 0 1 3.71 8.96l-7.07 7.07a.75.75 0 0 1-1.06 0l-7.07-7.07a5.25 5.25 0 1 1 7.42-7.42l.42.42.42-.42a5.25 5.25 0 0 1 3.63-1.54Z'/></svg>`;
+    favBtn.innerHTML = isFavourite(id) ? heartOutline() : filledHeart();
   });
   card.appendChild(favBtn);
 
---
+++ js/ui/icons/svg.js
@@ -0,0 +1,7 @@
+export function heartOutline() {
+  return `<svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' class='w-6 h-6 text-red-500'><path fill-rule='evenodd' d='M12.1 4.435c1.74-1.72 4.56-1.72 6.3 0 1.72 1.72 1.72 4.52 0 6.24l-6.07 6.06a.75.75 0 0 1-1.06 0l-6.07-6.06c-1.72-1.72-1.72-4.52 0-6.24 1.74-1.72 4.56-1.72 6.3 0Z' clip-rule='evenodd'/></svg>`;
+}
+
+export function filledHeart() {
+  return `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' class='w-6 h-6 text-red-500'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M16.5 3.75a5.25 5.25 0 0 1 3.71 8.96l-7.07 7.07a.75.75 0 0 1-1.06 0l-7.07-7.07a5.25 5.25 0 1 1 7.42-7.42l.42.42.42-.42a5.25 5.25 0 0 1 3.63-1.54Z'/></svg>`;
+}
